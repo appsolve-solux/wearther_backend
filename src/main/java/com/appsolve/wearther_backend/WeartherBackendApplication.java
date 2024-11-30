9b02afb079f1e7489fc18eef105de715da8171ac
yeonjae02 <0yeonjae2@naver.com>
2024-11-30 21:19:30 +0900

    init: 프로젝트 초기 세팅
    - 스프링부트 3.4.0, 자바 17
    - 필요한 의존성 추가

---
+++ src/main/java/com/appsolve/wearther_backend/WeartherBackendApplication.java
@@ -0,0 +1,13 @@
+package com.appsolve.wearther_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeartherBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeartherBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/appsolve/wearther_backend/WeartherBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appsolve.wearther_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeartherBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
