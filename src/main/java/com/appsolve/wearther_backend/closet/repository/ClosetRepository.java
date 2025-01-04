8cde688c39a1b4fe0ffe652b914f18ae3ac61797
yeonjae02 <0yeonjae2@naver.com>
2025-01-04 20:06:54 +0900

    feat: 추천 관련 레포지토리 코드 작성

--- src/main/java/com/appsolve/wearther_backend/closet/repository/ClosetRepository.java
+++ src/main/java/com/appsolve/wearther_backend/closet/repository/ClosetRepository.java
@@ -1,14 +1,9 @@
 package com.appsolve.wearther_backend.closet.repository;
 
 import com.appsolve.wearther_backend.closet.entity.Closet;
-import com.appsolve.wearther_backend.closet.entity.ClosetLower;
-import com.appsolve.wearther_backend.closet.entity.ClosetOther;
-import com.appsolve.wearther_backend.closet.entity.ClosetUpper;
-import com.appsolve.wearther_backend.member.Member;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -18,14 +13,4 @@ import java.util.Optional;
 public interface ClosetRepository extends JpaRepository<Closet, Long> {
     @Query("SELECT c FROM Closet c WHERE c.id = :id")
     Optional<Closet> findClosetById(Long id);
-
-    @Query("SELECT cu FROM ClosetUpper cu JOIN FETCH cu.closet c WHERE c.id = :closetId")
-    List<ClosetUpper> findClosetUppersByClosetId(Long closetId);
-
-    @Query("SELECT cl FROM ClosetLower cl JOIN FETCH cl.closet c WHERE c.id = :closetId")
-    List<ClosetLower> findClosetLowersByClosetId(Long closetId);
-
-    @Query("SELECT co FROM ClosetOther co JOIN FETCH co.closet c WHERE c.id = :closetId")
-    List<ClosetOther> findClosetOthersByClosetId(Long closetId);
-
 }
--- src/main/java/com/appsolve/wearther_backend/init_data/repository/TasteUpperWearRepository.java
+++ src/main/java/com/appsolve/wearther_backend/init_data/repository/TasteUpperWearRepository.java
@@ -4,6 +4,10 @@ import com.appsolve.wearther_backend.init_data.entity.TasteUpperWear;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
+import java.util.List;
+
 @Repository
 public interface TasteUpperWearRepository extends JpaRepository<TasteUpperWear, Long> {
+    List<TasteUpperWear> findByTasteId(Long tasteId);
 }
