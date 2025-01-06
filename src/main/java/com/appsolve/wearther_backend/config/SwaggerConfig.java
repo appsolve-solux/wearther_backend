bc4d876da22b3339965d8845cc396eb3c263d4ba
qlcsk <qlcskcode@naver.com>
2025-01-06 21:09:28 +0900

    Swagger에 Bearer 토큰 인증 추가
    JWT 이용을 위해 추가함

--- src/main/java/com/appsolve/wearther_backend/config/SwaggerConfig.java
+++ src/main/java/com/appsolve/wearther_backend/config/SwaggerConfig.java
@@ -2,6 +2,8 @@ package com.appsolve.wearther_backend.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,14 +12,28 @@ public class SwaggerConfig {
     private static final String API_NAME = "Appsolve";
     private static final String API_VERSION = "1.0.0";
     private static final String API_DESCRIPTION = "<h3>Appsolve API TEST</h3>";
+    private static final String SECURITY_SCHEME_NAME = "BearerAuth";
 
     @Bean
     public OpenAPI openAPI() {
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP) // HTTP 타입
+                .scheme("bearer") // Bearer 인증 방식
+                .bearerFormat("JWT"); // JWT 토큰 포맷 명시
+
+        // SecurityRequirement 설정
+        SecurityRequirement securityRequirement = new SecurityRequirement()
+                .addList(SECURITY_SCHEME_NAME); // SecurityScheme 이름 추가
+
+
         Info info = new Info()
                 .title(API_NAME)
                 .version(API_VERSION)
                 .description(API_DESCRIPTION);
 
-        return new OpenAPI().info(info);
-    }
+        return new OpenAPI()
+                .addSecurityItem(securityRequirement) // 보안 요구사항 추가
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes(SECURITY_SCHEME_NAME, securityScheme)) // SecuritySchemes 추가
+                .info(info);    }
 }
