be3dce41e73536a2146950fc57a63a0cea300a2d
seoyeonsw <c2c4@naver.com>
2025-01-06 23:34:33 +0900

    [feat] #12 취향 관련 레포지토리 코드 작성

--- src/main/java/com/appsolve/wearther_backend/Repository/MemberTasteRepository.java
+++ src/main/java/com/appsolve/wearther_backend/Repository/MemberTasteRepository.java
@@ -2,13 +2,17 @@ package com.appsolve.wearther_backend.Repository;
 
 import com.appsolve.wearther_backend.Entity.MemberTaste;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-@Repository
 public interface MemberTasteRepository extends JpaRepository<MemberTaste, Long> {
-    List<MemberTaste> findByMemberId(Long memberId); // 특정 사용자의 취향 조회
+    List<MemberTaste> findByMemberMemberId(Long memberId);
 
-    void deleteByMemberId(Long memberId); // 특정 사용자의 모든 취향 삭제
+    @Modifying
+    @Query("DELETE FROM MemberTaste mt WHERE mt.member.id = :memberId")
+    void deleteByMemberId(@Param("memberId") Long memberId);
 }
+
