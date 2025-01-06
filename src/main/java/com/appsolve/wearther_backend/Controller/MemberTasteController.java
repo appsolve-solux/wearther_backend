f8bf500a8810b3631bea020182f43f84eaf1b4e9
seoyeonsw <c2c4@naver.com>
2025-01-06 23:34:57 +0900

    [feat] #12 취향 관련 컨트롤러 코드 작성

--- src/main/java/com/appsolve/wearther_backend/Controller/MemberTasteController.java
+++ src/main/java/com/appsolve/wearther_backend/Controller/MemberTasteController.java
@@ -3,8 +3,6 @@ package com.appsolve.wearther_backend.Controller;
 
 import com.appsolve.wearther_backend.Service.MemberTasteService;
 import com.appsolve.wearther_backend.apiResponse.ApiResponse;
-import com.appsolve.wearther_backend.init_data.entity.Taste;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,15 +19,15 @@ public class MemberTasteController {
         this.memberTasteService = memberTasteService;
     }
 
-    @PatchMapping("/tastes/{memberId}")
+    @PatchMapping("/update/{memberId}")
     public ResponseEntity<ApiResponse<List<Long>>> updateTastes(@PathVariable Long memberId, @RequestBody List<Long> tasteIds) {
         memberTasteService.updateMemberTastes(memberId, tasteIds);
         return ApiResponse.success(HttpStatus.OK, tasteIds);
     }
 
-    @GetMapping("/{memberId}/tastes")
-    public ResponseEntity<ApiResponse<List<Taste>>> getTastes(@PathVariable Long memberId) {
-        List<Taste> tastes = memberTasteService.getMemberTastes(memberId);
+    @GetMapping("/get/{memberId}")
+    public ResponseEntity<ApiResponse<List<Long>>> getTastes(@PathVariable Long memberId) {
+        List<Long> tastes = memberTasteService.getMemberTasteIds(memberId);
         return ApiResponse.success(HttpStatus.OK, tastes);
     }
 }
