019f3161f017e67258f235eaad6ad5d754a1ac7a
seoyeonsw <c2c4@naver.com>
2025-01-10 04:06:55 +0900

    [feat] #25 즐겨찾기 들어갔을 때 보여지는 정보 관련 컨트롤러 코드 작성

--- src/main/java/com/appsolve/wearther_backend/Controller/LocationController.java
+++ src/main/java/com/appsolve/wearther_backend/Controller/LocationController.java
@@ -1,26 +1,34 @@
 package com.appsolve.wearther_backend.Controller;
 
+import com.appsolve.wearther_backend.Dto.LocationWeatherResponseDto;
 import com.appsolve.wearther_backend.Dto.LocationIndexUpdateRequestDto;
 import com.appsolve.wearther_backend.Dto.LocationPostRequestDto;
 import com.appsolve.wearther_backend.Entity.Location;
 import com.appsolve.wearther_backend.Service.LocationService;
+import com.appsolve.wearther_backend.Service.LocationWeatherService;
 import com.appsolve.wearther_backend.apiResponse.ApiResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/location")
 public class LocationController {
 
     @Autowired
     private LocationService locationService;
 
+    @Autowired
+    private LocationWeatherService locationWeatherService;
+
+
     @PostMapping("/post")
-    public ResponseEntity<ApiResponse<Location>> addLocation(@RequestBody LocationPostRequestDto locationRequest) {
+    public ResponseEntity<ApiResponse<LocationPostRequestDto>> addLocation(@RequestBody LocationPostRequestDto locationRequest) {
         Location location = locationService.addLocation(locationRequest);
-        return ApiResponse.success(HttpStatus.CREATED, location);
+        return ApiResponse.success(HttpStatus.CREATED, locationRequest);
     }
 
     @DeleteMapping("/delete/{memberId}/{locationIndex}")
@@ -34,4 +42,12 @@ public class LocationController {
         locationService.updateLocationIndex(updateRequest.getMemberId(), updateRequest.getBeforeLocationIndex(), updateRequest.getAfterLocationIndex());
         return ApiResponse.success(HttpStatus.OK, updateRequest);
     }
+
+    @GetMapping("/{memberId}")
+    public ResponseEntity<ApiResponse<LocationWeatherResponseDto>> getLocationList (@PathVariable Long memberId){
+        LocationWeatherResponseDto dto = locationWeatherService.getLocationsWeather(memberId);
+        return ApiResponse.success(HttpStatus.OK, dto);
+    }
+
+
 }
