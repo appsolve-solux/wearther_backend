8229ed1477973e37a1d19f3ead32cac290139f03
qlcsk <qlcskcode@naver.com>
2025-01-10 11:27:14 +0900

    필요없는 파일 삭제
    삭제

--- src/main/java/com/appsolve/wearther_backend/Dto/JwtTokenDto.java
+++
@@ -1,9 +0,0 @@
-package com.appsolve.wearther_backend.Dto;
-
-public class JwtTokenDto {
-    //jwt 인증타입 액세스 토큰 요청시 사용되는 인증 유형이다!
-    private String grantType;
-
-    private String accessToken;
-    private String refreshToken;
-}
