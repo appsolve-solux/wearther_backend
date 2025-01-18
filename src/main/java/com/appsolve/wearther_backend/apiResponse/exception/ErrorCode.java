24c124e6370f3f884033b87d90bd24f4ca46f26f
jungwoo <168719793+jungwoow@users.noreply.github.com>
2025-01-18 16:43:18 +0900

    충돌 해결

--- src/main/java/com/appsolve/wearther_backend/apiResponse/exception/ErrorCode.java
+++ src/main/java/com/appsolve/wearther_backend/apiResponse/exception/ErrorCode.java
@@ -33,7 +33,7 @@ public enum ErrorCode {
     DUPLICATE_LOCATION(HttpStatus.BAD_REQUEST, "LOCATION4001", "중복된 위치 정보입니다."),
     INDEX_NOT_FOUND(HttpStatus.BAD_REQUEST, "USER4003", "해당 사용자 정보에 존재하지 않는 인덱스입니다."),
     Weather_NOT_FOUND(HttpStatus.BAD_REQUEST, "USER4004", "날씨 정보를 가져올 수 없습니다"),
-    TEMP_NOT_FOUND(HttpStatus.BAD_REQUEST, "USER4005", "체감온도를 구할 수 없습니다");
+    TEMP_NOT_FOUND(HttpStatus.BAD_REQUEST, "USER4005", "체감온도를 구할 수 없습니다"),
 
     /* JWT 관련 에러 */
     INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "JWT4001", "유효하지 않은 JWT 토큰입니다."),
