89e317b143f616c1af2c227a131bce59ff9102dd
jungwoow <yeriel.390@gmail.com>
2025-01-19 17:56:41 +0900

    refactor: 토큰으로 받도록 수정

--- src/main/java/com/appsolve/wearther_backend/home/controller/HomeController.java
+++ src/main/java/com/appsolve/wearther_backend/home/controller/HomeController.java
@@ -1,41 +1,41 @@
 package com.appsolve.wearther_backend.home.controller;
 
+import com.appsolve.wearther_backend.Entity.MemberEntity;
 import com.appsolve.wearther_backend.apiResponse.ApiResponse;
+import com.appsolve.wearther_backend.auth.Service.AuthService;
 import com.appsolve.wearther_backend.home.dto.RecommendResponseDto;
 import com.appsolve.wearther_backend.home.dto.WeatherResponseDto;
 import com.appsolve.wearther_backend.home.service.HomeWeatherService;
 import com.appsolve.wearther_backend.home.service.TodayRecommendService;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequiredArgsConstructor
 @RequestMapping("/home")
 public class HomeController {
+    private final HomeWeatherService homeWeatherService;
+    private final TodayRecommendService todayRecommendService;
+    private final AuthService authService;
 
-    @Autowired
-    private HomeWeatherService HomeWeatherService;
-
-    @Autowired
-    private TodayRecommendService TodayRecommendService;
+    public HomeController(HomeWeatherService homeWeatherService, TodayRecommendService todayRecommendService, AuthService authService) {
+        this.homeWeatherService = homeWeatherService;
+        this.todayRecommendService = todayRecommendService;
+        this.authService = authService;
+    }
 
     @GetMapping("/weather/{latitude}/{longitude}")
     public ResponseEntity<ApiResponse<WeatherResponseDto>> getCurrentWeather (@PathVariable Double latitude, @PathVariable Double longitude){
-        WeatherResponseDto currentWeather = HomeWeatherService.getWeatherValue(latitude, longitude);
+        WeatherResponseDto currentWeather = homeWeatherService.getWeatherValue(latitude, longitude);
         return ApiResponse.success(HttpStatus.OK, currentWeather);
     }
 
-    @GetMapping("/recommend/{memberId}/{latitude}/{longitude}")
-    public ResponseEntity<ApiResponse<RecommendResponseDto>> getCurrentWeather (@PathVariable Long memberId, @PathVariable Double latitude, @PathVariable Double longitude){
-        RecommendResponseDto todayRecommend = TodayRecommendService.getTodayRecommend(memberId, latitude, longitude);
+    @GetMapping("/recommend/{latitude}/{longitude}")
+    public ResponseEntity<ApiResponse<RecommendResponseDto>> getTodayRecommend (@RequestHeader("Authorization") String token, @PathVariable Double latitude, @PathVariable Double longitude){
+        MemberEntity member =  authService.getMemberEntityFromToken(token);
+        Long memberId = member.getMemberId();
+        RecommendResponseDto todayRecommend = todayRecommendService.getTodayRecommend(memberId, latitude, longitude);
         return ApiResponse.success(HttpStatus.OK, todayRecommend);
     }
-
 }
